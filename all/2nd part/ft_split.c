f88917e22722639af74ee3b93745d6de9d7a4c43
TheGreatSoider <colindumc99@gmail.com>
2022-08-22 00:56:34 +0800

    ft_split

--- all/2nd part/ft_split.c
+++ all/2nd part/ft_split.c
@@ -10,9 +10,42 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <libft.h>
+//#include <libft.h>
+
+//s = 0123456789876543210 || 000 11 2 3333 4 5 6666666 7777 88 99999 88888 77 66 54 422 3111 22 11 011
+//c = 9 || ' ' && '9'
+//
 
 char **ft_split(char const *s, char c)
 {
-	
+	int		i;
+	int 	j;
+	char	**str;
+
+	i = 0;
+	if (s == 0)
+		return ('\0');
+	while (s[i] != '\0')
+	{
+		j = 0;
+		if (s[i] != c)
+			j++;
+		i++;
+	}
+	*str = (char **)malloc(sizeof(char *) * (j + 1);
+	if (str == 0)
+		return ('\0');
+	i = 0;
+	j = 0;
+	while (s[i] != '\0')
+	{
+		while (s[i] != c)
+		{
+			str[j] = s[i];
+			j++;
+			i++;
+		}
+		i++;
+	}
+	return (*str);
 }
