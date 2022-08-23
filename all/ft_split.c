192a85c08283faede9301533d99d693385222804
TheGreatSoider <colindumc99@gmail.com>
2022-08-24 00:26:39 +0800

    ft_split

--- all/ft_split.c
+++ all/ft_split.c
@@ -10,42 +10,77 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-//#include "libft.h"
-#include <stdlib.h>
-#include <stdio.h>
+#include "libft.h"
+// #include <stdlib.h>
+// #include <stdio.h>
+// #include <stddef.h>
 
-//s = 0123456789876543210 || 000 11 2 3333 4 5 6666666 7777 88 99999 88888 77 66 54 422 3111 22 11 011
-//c = 9 || ' ' && '9'
+//s = 0123456789876543210
+//c = 9 
 //
+char	*ft_substr(const char *s, unsigned int start, size_t len)
+{
+	size_t	i;
+	size_t	j;
+	char	*str;
+
+	str = (char *)malloc(sizeof(*s) * (len + 1));
+	if (!str)
+		return (NULL);
+	i = 0;
+	j = 0;
+	while (s[i] != '\0')
+	{
+		if (i >= start && j < len)
+		{
+			str[j] = s[i];
+			j++;
+		}
+		i++;
+	}
+	str[j] = '\0';
+	return (str);
+}
 
 char **ft_split(const char *s, char c)
 {
 	int		i;
 	int 	j;
+	int		l;
 	char	**str;
 
 	i = 0;
+	j = 0;
 	if (s == 0)
 		return ('\0');
 	while (s[i] != '\0')
 	{
-		j = 0;
 		if (s[i] != c)
+		{
+			while (s[i] != c)
+				i++;
 			j++;
+		}
 		i++;
 	}
-	str = (char **)malloc(sizeof(char *) * (j + 1));
+	str = (char **)malloc(sizeof(char *) * (j + 1)); 
 	if (*str == 0)
 		return ('\0');
 	i = 0;
-	j = 0;
+
+	l = 0;
 	while (s[i] != '\0')
 	{
-		while (s[i] != c)
+		if (s[i] != c)
 		{
-			*str[j] = s[i];
-			j++;
-			i++;
+			j = i;
+			while (s[i] != c)
+				j++;
+			while (l != j)
+				str[++l] = ft_substr(s, i, j);
+			str[l] = 0;
+			l = 0;
+			i = j;
 		}
 		i++;
 	}
@@ -54,9 +89,11 @@ char **ft_split(const char *s, char c)
 
 // int main (void)
 // {
+// 	char **sstr;
 // 	char	str[] = "Matthew like Jay Chou according to Colin Cheong.";
 // 	char	character = ' ';
 
-// 	printf("%s\n", ft_split(str, character)[0]);
+// 	sstr = ft_split(str, character);
+// 	printf("%s\n", *sstr);
 // 	return (0);
 // }
