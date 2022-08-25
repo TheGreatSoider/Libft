506f3a1ada324e4b29686b461bc09111924f46f9
TheGreatSoider <colindumc99@gmail.com>
2022-08-25 23:32:31 +0800

    adjusted

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: cocheong <cocheong@student.42kl.my>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/05 14:39:41 by cocheong          #+#    #+#             */
-/*   Updated: 2022/08/15 21:07:28 by cocheong         ###   ########.fr       */
+/*   Updated: 2022/08/25 23:32:17 by cocheong         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,11 @@ void	*ft_memcpy(void *restrict dest, void const *restrict src, size_t n)
 	size_t	i;
 
 	i = 0;
+	if (!dest || !src)
+		return (0);
 	while (i < n)
 	{
-		*(char *)(dest + i) = *(char *)(src + i);
+		*((char *)(dest + i)) = *((char *)(src + i));
 		i++;
 	}
 	return (dest);
