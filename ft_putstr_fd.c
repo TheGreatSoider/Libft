0246aa345d71365578720c382d3216afbec6190e
TheGreatSoider <colindumc99@gmail.com>
2022-08-26 20:43:58 +0800

    space

--- ft_putstr_fd.c
+++ ft_putstr_fd.c
@@ -6,7 +6,7 @@
 /*   By: cocheong <cocheong@student.42kl.my>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/20 22:11:23 by cocheong          #+#    #+#             */
-/*   Updated: 2022/08/26 20:12:00 by cocheong         ###   ########.fr       */
+/*   Updated: 2022/08/26 20:34:22 by cocheong         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 void	ft_putstr_fd(char *s, int fd)
 {
 	int	i;
-	
+
 	if (s != NULL)
 		i = ft_strlen(s);
 	if (s == 0)
