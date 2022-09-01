490f5bea5db5775a2eab756b6ab7f439e6016911
TheGreatSoider <colindumc99@gmail.com>
2022-09-01 16:16:55 +0800

    fixed calloc

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: cocheong <cocheong@student.42kl.my>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/15 19:47:45 by cocheong          #+#    #+#             */
-/*   Updated: 2022/08/25 16:45:39 by cocheong         ###   ########.fr       */
+/*   Updated: 2022/09/01 16:07:35 by cocheong         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,9 @@ void	*ft_calloc(size_t count, size_t size)
 	size_t	i;
 
 	i = size * count;
-	if (count != i / size)
+	if (i == 0)
+		return (NULL);
+	if (count != (i / size))
 		return (NULL);
 	rtn = malloc(i);
 	if (rtn == 0)
